6420fb023f895ed90dc7f2b3e18a56c9596bd344
unknown <mwahbtalents@gmail.com>
2024-01-05 23:37:19 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let img =document.querySelector(".img");
+let container=document.querySelector(".container")
+function phome(phome){
+    img.src=phome;
+}
+function colors(color){
+    container.style.background=color;
+
+}
